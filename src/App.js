3c8d761b08670f792f1e373ab7523f0caf5a142a
tuannhan <tuannhan6@gmail.com>
2022-08-04 23:02:28 +0700

    init

---
+++ src/App.js
@@ -0,0 +1,11 @@
+
+function App() {
+  return (
+    <div className="bg-purple-500">
+      <h1 className='text-xl'>Hello</h1>
+     <button className='btn'>Click</button>
+    </div>
+  );
+}
+
+export default App;
