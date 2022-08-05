3c4c59d5ea438265235a62e48b8d048d892b38c6
tuannhan <nhan.nvtn@tctav.com>
2022-08-05 14:06:11 +0700

    UserList component

---
+++ src/components/users/UserResults.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import { useEffect, useState } from 'react'
+
+function UserResults() {
+
+    const [users, setUsers] = useState([])
+    const [isLoading, setIsLoading] = useState(true)
+
+    useEffect(() => {
+        fetchUser()
+    }, [])
+    const fetchUser = async () => {
+        const response = await fetch(`${process.env.REACT_APP_GITHUB_URL}/users`, {
+            headers: {
+                'Authorization': `token ${process.env.REACT_APP_GITHUB_TOKEN}`
+            }
+        }) 
+
+        const data = await response.json()
+
+        setUsers(data)
+        setIsLoading(false)
+    }
+
+    if (!isLoading) {
+        return (
+          <div className='grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2'>
+              {users.map((user) => (
+                  <h3 key={user.id}>{user.login}</h3>
+              ))}
+          </div>
+        )
+    } else {
+        return <h3>Loading...</h3>
+    }
+}
+
+export default UserResults
