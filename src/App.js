303050140255f9e52a4fc08aeb0495bb886f8fd2
tuannhan <tuannhan6@gmail.com>
2022-08-08 21:08:44 +0700

    Alert component

--- src/App.js
+++ src/App.js
@@ -1,30 +1,33 @@
-import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
-import Navbar from './components/layout/Navbar';
-import Footer from './components/layout/Footer';
-import Home from './pages/Home';
-import About from './pages/About';
-import NotFound from './pages/NotFound';
-import {GithubProvider} from './context/github/GithubContext'
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Navbar from "./components/layout/Navbar";
+import Footer from "./components/layout/Footer";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import NotFound from "./pages/NotFound";
+import { GithubProvider } from "./context/github/GithubContext";
+import { AlertProvider } from "./context/alert/AlertContext";
+import Alert from "./components/layout/Alert";
 
 function App() {
   return (
     <GithubProvider>
-      <Router>
-      <div className='flex flex-col justify-between h-screen'>
-        <Navbar>
-
-        </Navbar>
-        <main className='container mx-auto px-3 pb-12'>
-          <Routes>
-            <Route path='/' element={<Home/>}></Route>
-            <Route path='/about' element={<About/>}></Route>
-            <Route path='/notfound' element={<NotFound/>}></Route>
-            <Route path='/*' element={<NotFound/>}></Route>
-          </Routes>
-        </main>
-        <Footer></Footer>
-      </div>
-    </Router>
+      <AlertProvider>
+        <Router>
+          <div className="flex flex-col justify-between h-screen">
+            <Navbar></Navbar>
+            <main className="container mx-auto px-3 pb-12">
+              <Alert></Alert>
+              <Routes>
+                <Route path="/" element={<Home />}></Route>
+                <Route path="/about" element={<About />}></Route>
+                <Route path="/notfound" element={<NotFound />}></Route>
+                <Route path="/*" element={<NotFound />}></Route>
+              </Routes>
+            </main>
+            <Footer></Footer>
+          </div>
+        </Router>
+      </AlertProvider>
     </GithubProvider>
   );
 }
--- src/components/users/UserSearch.jsx
+++ src/components/users/UserSearch.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useState, useContext } from 'react'
 import GithubContext from '../../context/github/GithubContext'
+import AlertContext from '../../context/alert/AlertContext'
 
 function UserSearch() {
 
@@ -11,11 +12,12 @@ function UserSearch() {
     }
 
     const {users, searchUsers, clearUsers} = useContext(GithubContext)
+    const {setAlert} = useContext(AlertContext)
 
     const handleSubmit = (e) => {
         e.preventDefault()
         if (text === '') {
-            alert('Please enter something')
+            setAlert('Please enter something', 'error')
         } else {
             searchUsers(text)
             setText('')
